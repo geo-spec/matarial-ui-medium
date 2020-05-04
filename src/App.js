0405593582c5e6fe8e185dc5668f0c3790ff300e
Georgy <shilingeorgiy@yandex.ru>
2020-05-04 21:46:43 +0300

    temp commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import NavBar from './components/NavBar'
 import CourseList from './components/CourseList'
+// import Paperbase from "./paperbase/Paperbase";
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
         <CourseList />
+        {/*<Paperbase />*/}
       </div>
     )
   }
